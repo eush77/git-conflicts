35f7124db8d0ac9027ca9b7e0667fa5236bc954f
Eugene Sharygin <eush77@gmail.com>
2015-06-16 10:19:12 +0300

    Autodetect unmerged paths

--- cli.js
+++ cli.js
@@ -4,13 +4,20 @@
 var stringReplace = require('./lib/string-replace');
 
 var help = require('help-version')(usage()).help,
-    edit = require('string-editor');
+    edit = require('string-editor'),
+    split = require('transform-split');
 
-var fs = require('fs');
+var fs = require('fs'),
+    spawn = require('child_process').spawn;
 
 
 function usage() {
-  return 'Usage:  git conflicts [file]...';
+  return [
+    'Usage:  git conflicts [file]...',
+    '',
+    'Run conflict resolution for each file in order.',
+    'With no arguments, run conflict resolution for each unmerged path.'
+  ].join('\n');
 }
 
 
@@ -56,5 +63,25 @@ var enqueue = (function () {
 
 
 (function (argv) {
-  argv.forEach(enqueue);
+  if (argv.length) {
+    argv.forEach(enqueue);
+  }
+  else {
+    var git = spawn('git', ['ls-files', '--unmerged'], {
+      stdio: ['ignore', 'pipe', process.stderr]
+    });
+
+    git.stdout
+      .pipe(split())
+      .on('data', function (chunk) {
+        var filename = chunk.toString().split(/\s+/)[3];
+        enqueue(filename);
+      });
+
+    git.on('exit', function (code, signal) {
+      if (code !== 0) {
+        throw Error('git terminated with error');
+      }
+    });
+  }
 }(process.argv.slice(2)));
