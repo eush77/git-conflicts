2f0289360e2a158e68eef0aa03501dc3d63002bc
Eugene Sharygin <eush77@gmail.com>
2015-11-26 01:35:31 +0300

    resolution: check for unresolved conflicts

--- test/resolution.js
+++ test/resolution.js
@@ -13,11 +13,21 @@ test('resolution', function (t) {
     '<<<<<<< 2b0b56a7b74d4f5a0a7ef042c34eb27981ab9859\n',
     'Final version.\n',
     'All conflicts fixed.\n',
-    '>>>>>>> Refactor\n'
+    '>>>>>>> Refactor'
   ].join(''), next(function (err, data) {
     t.error(err);
     t.equal(data, 'Final version.\nAll conflicts fixed.',
             'successful resolution');
+  }));
+
+  resolution.extract([
+    '<<<<<<< 2b0b56a7b74d4f5a0a7ef042c34eb27981ab9859\n',
+    'Can optionally be newline-terminated.\n',
+    '>>>>>>> Refactor\n'
+  ].join(''), next(function (err, data) {
+    t.error(err);
+    t.equal(data, 'Can optionally be newline-terminated.',
+            'trailing newline');
   }));
 
   resolution.extract([
@@ -31,5 +41,15 @@ test('resolution', function (t) {
     'Both markers, actually.\n'
   ].join(''), next(function (err) {
     t.true(err, 'no conflict markers');
+  }));
+
+  resolution.extract([
+    '<<<<<<< 2b0b56a7b74d4f5a0a7ef042c34eb27981ab9859\n',
+    'Version before.\n',
+    '=======\n',
+    'Version after.\n',
+    '>>>>>>> Refactor\n'
+  ].join(''), next(function (err) {
+    t.true(err, 'unresolved conflict');
   }));
 });
