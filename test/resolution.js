275369e1326fb91102e53116f25cbe05c5c694f5
Eugene Sharygin <eush77@gmail.com>
2015-11-26 02:08:56 +0300

    resolution: support comments and skip newlines

--- test/resolution.js
+++ test/resolution.js
@@ -31,6 +31,37 @@ test('resolution', function (t) {
   }));
 
   resolution.extract([
+    '# Comments are meant to be used by us, to embed some help on how\n',
+    '# to use the tool in the tool\'s control document. At the moment of\n',
+    '# writing, we just dump one conflict after another and shell out\n',
+    '# to user\'s editor, and all user is expected to know he or she is\n',
+    '# expected to do.\n',
+    '\n',
+    '# We could devise some special syntax just for that, such as YAML\n',
+    '# frontmatter or some other type of header, but it is better and\n',
+    '# simpler to design a general purpose mechanism instead.\n',
+    '# Orthogonality law, applied.\n',
+    '\n',
+    '# This also follows the Rule of Least Surprise, because Git and other\n',
+    '# tools do something like that, too.\n',
+    '\n',
+    '<<<<<<< 2b0b56a7b74d4f5a0a7ef042c34eb27981ab9859\n',
+    'Resolved.\n',
+    '# This is not a comment, but rather part of the diff.\n',
+    '# Host languages have comments, too!\n',
+    '>>>>>>> Refactor\n',
+    '\n',
+    '# Nice!\n'
+  ].join(''), next(function (err, data) {
+    t.error(err);
+    t.equal(data, [
+      'Resolved.',
+      '# This is not a comment, but rather part of the diff.',
+      '# Host languages have comments, too!'
+    ].join('\n'), 'comments');
+  }));
+
+  resolution.extract([
     'Conflicts markers are required.\n'
   ].join(''), next(function (err) {
     t.true(err, 'no conflict markers');
