6c4065c5ee26f90f52af5f4d68318939a846ceba
Eugene Sharygin <eush77@gmail.com>
2016-01-11 21:42:49 +0300

    refactor: decouple conflict resolution from the CLI

--- cli.js
+++ cli.js
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
 'use strict';
 
-var conflictRegExp = require('./lib/conflict'),
-    resolution = require('./lib/resolution'),
-    ResolutionError = require('./lib/resolution-error');
+var resolveFile = require('./lib/resolve-file');
 
 var help = require('help-version')(usage()).help,
-    edit = require('string-editor'),
     byline = require('byline'),
-    cloneRegExp = require('clone-regexp'),
-    prompt = require('inquirer').prompt,
+    Queue = require('push-queue'),
+    sliceArgs = require('impartial'),
     chalk = require('chalk'),
-    debug = require('debug')('git-conflicts');
+    debug = require('debug')('git-conflicts'),
+    edit = require('string-editor'),
+    prompt = require('inquirer').prompt;
 
-var fs = require('fs'),
-    spawn = require('child_process').spawn;
+var spawn = require('child_process').spawn;
 
 
 function usage() {
@@ -27,13 +25,41 @@ function usage() {
 }
 
 
+// Handle error.
+function error (err) {
+  if (debug.enabled) {
+    throw err;
+  }
+
+  console.error(chalk.red(err.name) + ': ' + err.message);
+  process.exit(1);
+}
+
+
 (function main (argv) {
-  var enqueue = newQueue();
+  // Set up resolution callbacks.
+  resolveFile = resolveFile.bind(null, {
+    resolve: function (filename, conflict, cb) {
+      edit(prepareConflictForEditing(filename, conflict),
+           Date.now() + '.diff', cb);
+    },
+    onResolutionError: onResolutionError
+  });
+
+  // Create resolution queue.
+  var enqueue = Queue(function (filename, cb) {
+    resolveFile(filename, function (err) {
+      if (err) return error(err);
+      cb();
+    });
+  });
 
   if (argv.length) {
-    argv.forEach(enqueue);
+    // Get list of files from the command line.
+    argv.forEach(sliceArgs(enqueue, 0, 1));
   }
   else {
+    // Get list of files from Git.
     var git = spawn('git', ['ls-files', '--unmerged'], {
       stdio: ['ignore', 'pipe', process.stderr]
     });
@@ -59,81 +85,6 @@ function usage() {
     });
   }
 }(process.argv.slice(2)));
-
-
-// Handle error.
-function error (err) {
-  if (debug.enabled) {
-    throw err;
-  }
-
-  console.error(chalk.red(err.name) + ': ' + err.message);
-  process.exit(1);
-}
-
-
-// Create new queue for conflicted filenames.
-// Calls `resolveConflicts` in sequence on these files.
-function newQueue () {
-  var queue = [];
-
-  var check = function () {
-    if (queue.length) {
-      resolveConflicts(queue[0], function (err) {
-        if (err) return error(err);
-        queue.shift();
-        check();
-      });
-    }
-  };
-
-  return function (entry) {
-    queue.push(entry);
-    if (queue.length == 1) {
-      check();
-    }
-  };
-}
-
-
-// Read the file and resolve the conflicts one by one.
-function resolveConflicts (filename, cb) {
-  var diff = fs.readFileSync(filename, { encoding: 'utf8' });
-  var regexp = cloneRegExp(conflictRegExp, { multiline: true });
-
-  (function resolve (diff, merged) {
-    var match = diff.match(regexp);
-    if (!match) return cb();
-
-    resolveOne(match[0], function (err, result, retry) {
-      if (err) return cb(err);
-
-      if (retry) {
-        return resolve(diff, merged);
-      }
-
-      merged += diff.slice(0, match.index) + result;
-      diff = diff.slice(match.index + match[0].length);
-
-      fs.writeFileSync(filename, merged + diff);
-      resolve(diff, merged);
-    });
-  }(diff, ''));
-
-  function resolveOne (conflict, cb) {
-    var tempFileName = Date.now() + '.diff';
-    edit(prepareConflictForEditing(filename, conflict), tempFileName,
-         function (err, result) {
-           if (err) return cb(err);
-
-           resolution(result, function (err, result) {
-             return (err instanceof ResolutionError)
-               ? onResolutionError(filename, conflict, err, cb)
-               : cb(err, result);
-           });
-         });
-  }
-}
 
 
 // Add some comments explaining to user how to proceed with the conflict.
